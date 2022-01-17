f4062c8ab20bcc88f16214e495a5e04863721945
MANIKDVN <89959327+MANIKDVN@users.noreply.github.com>
2022-01-17 14:49:17 -0500

    Time: 1012 ms (36.66%), Space: 0B (100.00%) - LeetHub

---
+++ 1532-the-most-recent-three-orders/1532-the-most-recent-three-orders.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+select
+customer_name,customer_id,order_id,order_date
+from(
+select
+c.name as customer_name
+,o.customer_id
+,o.order_id
+,o.order_date
+,rank() over(partition by o.customer_id order by o.order_date desc) as rnk
+from Orders o left join Customers c
+using(customer_id)
+    ) as a
+where rnk<=3
+order by customer_name asc,customer_id asc,order_date desc
