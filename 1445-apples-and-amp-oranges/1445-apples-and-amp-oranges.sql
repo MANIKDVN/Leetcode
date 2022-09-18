a12e609a0a2c14055b4def4a606532872f1e8378
Manik Dhawan <89959327+MANIKDVN@users.noreply.github.com>
2022-09-18 00:58:50 -0400

    Time: 413 ms (45.53%), Space: 0B (100.00%) - LeetHub

--- 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
+++ 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
@@ -1,16 +1,12 @@
 # Write your MySQL query statement below
 select
 sale_date
-,sold_num_apples-sold_num_oranges as diff
+,apple-orange as diff
 from(
 select
 sale_date
-,sum(case when fruit='apples' then sold_num
-else 0 end) as sold_num_apples
-,sum(case when fruit='oranges' then sold_num
-else 0 end) as sold_num_oranges
-from
-Sales
-group by sale_date) as a
-order by sale_date
-
+,sum(case when fruit='apples' then sold_num else 0 end) as apple
+,sum(case when fruit='oranges' then sold_num else 0 end) as orange
+from Sales
+group by 1)
+as a
